b4df3fbeb0623f7adcd883ea1bf56a7e95883179
Alexey Vasin <vasin-a.i@yandex.ru>
2025-08-29 07:41:34 +0300

    chore: remove unused imports

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import java.time.Duration;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
